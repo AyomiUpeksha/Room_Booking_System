923d69bf1ed5b5cb9b0f8254d1d646129e2a7901
AyomiUpeksha <69380485+AyomiUpeksha@users.noreply.github.com>
2022-12-04 14:04:59 +0530

    Add files via upload

---
+++ databasegui/MyMain.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package databasegui;
+
+/**
+ *
+ * @author Dell
+ */
+public class MyMain {
+    public static void main(String args[])
+    {
+        
+        log obj1=new log();
+        obj1.setVisible(true);
+        
+    }
+    
+}
